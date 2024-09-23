6c141f7c85c54c0308d052d543d5f874569e4fde
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2024-09-23 16:29:07 -0400

    add settings for deployment

--- cs412/settings.py
+++ cs412/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r!-2#dvd3naopz*#su3-udn7jn3f(zeb7c5j-sm^o)fzrd2szq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
