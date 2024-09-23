6cc68b42a1a8c7d5fb25fa650ee64440217c78a6
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2024-09-23 19:35:19 -0400

    undo change

--- cs412/settings.py
+++ cs412/settings.py
@@ -132,8 +132,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 STORAGES = {
     # ...
     "staticfiles": {
