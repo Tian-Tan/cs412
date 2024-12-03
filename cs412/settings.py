d10e95474246e1cc0b5df3c6f7a628883a7275d8
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2024-12-03 11:45:18 -0500

    update static url

--- cs412/settings.py
+++ cs412/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 import os #operating systems library
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
