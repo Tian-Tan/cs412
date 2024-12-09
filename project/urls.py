c2458d17fc0ad570a8db8e4ac976fb68392ad050
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2024-12-08 20:01:26 -0500

    add homepage and book detail page

--- project/urls.py
+++ project/urls.py
@@ -7,8 +7,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     ## all the urls to this app
-    path(r'', views.ShowAllBooksView.as_view(), name="show_all_books"),
+    path(r'', views.HomepageView.as_view(), name="home"),
     path(r'all_profiles', views.ShowAllProfilesView.as_view(), name="show_all_profiles"),
+    path(r'all_books', views.ShowAllBooksView.as_view(), name="show_all_books"),
+    path(r'book/<int:pk>', views.ShowBookView.as_view(), name="show_book"),
 
     # authentication URLs
     # path(r'login/', auth_views.LoginView.as_view(template_name='project/login.html'), name="login"),
--- project/views.py
+++ project/views.py
@@ -4,13 +4,28 @@
 from django.shortcuts import render
 
 # additional imports
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, TemplateView
 
 # import other components
 from .models import *
 from .forms import *
 
 # Create your views here.
+class HomepageView(TemplateView):
+    ''' A view to show a homepage template
+    '''
+    template_name = 'project/homepage.html'
+
+    def get_context_data(self, **kwargs):
+        ''' Add book count and user count to context data
+        '''
+        # Get the default context data
+        context = super().get_context_data(**kwargs)
+        # Add additional data for book count and user count
+        context['book_count'] = Book.objects.count()
+        context['user_count'] = User.objects.count()
+        return context
+
 class ShowAllBooksView(ListView):
     ''' A view to show all Books currently available
     '''
@@ -24,3 +39,10 @@ class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'project/show_all_profiles.html'
     context_object_name = 'profiles'
+
+class ShowBookView(DetailView):
+    ''' A view to show a single Book
+    '''
+    model = Book
+    template_name = 'project/show_book.html'
+    context_object_name = 'book'
