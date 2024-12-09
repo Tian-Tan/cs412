801bc75e4f45909ea7a11944cbdb55ddc3d56053
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2024-12-08 20:54:59 -0500

    update show all profiles page and add login logout functions

--- project/urls.py
+++ project/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path(r'book/<int:pk>', views.ShowBookView.as_view(), name="show_book"),
 
     # authentication URLs
-    # path(r'login/', auth_views.LoginView.as_view(template_name='project/login.html'), name="login"),
-    # path(r'logout/', auth_views.LogoutView.as_view(template_name='project/logged_out.html'), name="logout"),
+    path(r'login/', auth_views.LoginView.as_view(template_name='project/login.html'), name="login"),
+    path(r'logout/', auth_views.LogoutView.as_view(template_name='project/logged_out.html'), name="logout"),
     # path(r'register/', views.RegistrationView.as_view(), name='register'),
 ]
